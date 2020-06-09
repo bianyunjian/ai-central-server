f6278d35afaf98f599bab5a823bff2a89d64d6a1
xi.zhang <xi.zhang@hankutech.com>
2020-06-09 13:33:12 +0800

    add: 新增生产环境日志文件配置
    link #I1JXG8

--- src/main/java/com/hankutech/ax/centralserver/CentralServerApplication.java
+++ src/main/java/com/hankutech/ax/centralserver/CentralServerApplication.java
@@ -3,6 +3,11 @@ package com.hankutech.ax.centralserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 艾信智慧医疗中心服务
+ *
+ * @author ZhangXi
+ */
 @SpringBootApplication
 public class CentralServerApplication {
 
