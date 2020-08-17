531b3ae6b79663a1d5dcee915ab575057fc6ed33
卞云健 <yunjian.bian@hankutech.com>
2020-08-17 11:50:45 +0800

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,11 @@
-#FROM java:8
-#
-#COPY target/central-server-0.0.1-SNAPSHOT.jar /app.jar
-#COPY sdk /sdk
-#ENV TimeZone Asia/Shanghai
-#
-#RUN bash -c  'sh /sdk/deployLibraries.sh'
-#
-##RUN bash -c  'touch /app.jar'
-#EXPOSE 9000
-#ENTRYPOINT ["sh","-c","java ${JAVA_OPTS} -jar /app.jar ${EXT_CONFIG}"]
-
-FROM ax-base:1.0
+FROM java:8
 
 ENV TimeZone Asia/Shanghai
+
+COPY sdk /sdk
+
+RUN bash -c 'sh /sdk/deployLibraries.sh'
+ 
 
 COPY target/central-server-0.0.1-SNAPSHOT.jar /app.jar
 
--- src/test/java/com/hankutech/ax/centralserver/testUnicode.java
+++ src/test/java/com/hankutech/ax/centralserver/testUnicode.java
@@ -20,10 +20,34 @@ public class testUnicode {
         String receivedString = new String(byteData, charsetName);
         System.out.println(receivedString);
 
-        byte[] byteData2 =new byte[]{-46,-69};
+        byte[] byteData2 = new byte[]{-46, -69};
         String receivedString2 = new String(byteData2, charsetName);
         System.out.println(receivedString2);
 
 
+    }
+
+    @Test
+    void printZS() {
+//        用来打印出 1~100内的质数
+        int start = 1;
+        int end = 100;
+        if (start == 1) {
+            start = 2;
+        }
+        boolean notZS = false;
+        for (int i = start; i <= end; i++) {
+            notZS = false;
+            for (int j = 2; j <= i / 2; j++) {
+
+                if (i % j == 0) {
+                    notZS = true;
+                    break;
+                }
+            }
+            if (notZS == false) {
+                System.out.println(i);
+            }
+        }
     }
 }
